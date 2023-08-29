b006e1c9e042e56ca8dc7ade11d225f01cea289d
ArchitektRadim <45514318+ArchitektRadim@users.noreply.github.com>
2023-08-29 10:59:50 +0200

    Update gtfs_to_mysql.sql

--- gtfs_to_mysql.sql
+++ gtfs_to_mysql.sql
@@ -7,6 +7,7 @@ CREATE TABLE `agency` (
   agency_name VARCHAR(255),
   agency_url VARCHAR(255),
   agency_timezone VARCHAR(50),
+  agency_lang VARCHAR(8),
   agency_phone VARCHAR(50)
 );
 
@@ -30,40 +31,24 @@ DROP TABLE IF EXISTS calendar_dates;
 
 CREATE TABLE `calendar_dates` (
   service_id INT(11),
-  `date` VARCHAR(8),
+  date VARCHAR(8),
   exception_type INT(2),
   KEY `service_id` (service_id),
   KEY `exception_type` (exception_type)
 );
 
-DROP TABLE IF EXISTS fare_attributes;
-
-CREATE TABLE `fare_attributes` (
-  fare_id INT(11),
-  price DECIMAL(9,6),
-  currency_type VARCHAR(8),
-  payment_method INT(11),
-  transfers INT(11),
-  KEY `fare_id` (fare_id)
-);
-
-DROP TABLE IF EXISTS fare_rules;
-
-CREATE TABLE `fare_rules` (
-  fare_id INT(11),
-  route_id INT(11),
-  KEY `fare_id` (fare_id),
-  KEY `route_id` (route_id)
-);
-
-
 DROP TABLE IF EXISTS routes;
 
 CREATE TABLE `routes` (
   route_id INT(11) PRIMARY KEY,
+  agency_id int(11),
   route_short_name VARCHAR(50),
   route_long_name VARCHAR(255),
   route_type INT(2),
+  dir_1_from VARCHAR(50),
+  dir_1_to VARCHAR(50),
+  dir_2_from VARCHAR(50),
+  dir_2_to VARCHAR(50),
   KEY `route_type` (route_type)
 );
 
@@ -85,6 +70,9 @@ CREATE TABLE `stop_times` (
   departure_time VARCHAR(8),
   stop_id INT(11),
   stop_sequence INT(11),
+  pickup_type INT(11),
+  drop_off_type INT(11),
+  original_stop_id INT(11),
   KEY `trip_id` (trip_id),
   KEY `stop_id` (stop_id),
   KEY `stop_sequence` (stop_sequence)
@@ -94,9 +82,14 @@ DROP TABLE IF EXISTS stops;
 
 CREATE TABLE `stops` (
   stop_id INT(11) PRIMARY KEY,
+  stop_code VARCHAR(50),
   stop_name VARCHAR(255),
   stop_lat DECIMAL(9,6),
   stop_lon DECIMAL(9,6),
+  zone_id INT(11),
+  location_type INT(11),
+  wheelchair_boarding INT(11),
+  original_stop_id INT(11),
   KEY `stop_lat` (stop_lat),
   KEY `stop_lon` (stop_lon)
 );
@@ -107,13 +100,12 @@ CREATE TABLE `trips` (
   route_id INT(11),
   service_id INT(11),
   trip_id INT(11) PRIMARY KEY,
-  trip_headsign VARCHAR(255),
+  trip_short_name VARCHAR(50),
   direction_id TINYINT(1),
-  shape_id VARCHAR(50),
+  wheelchair_accessible INT(11),
   KEY `route_id` (route_id),
   KEY `service_id` (service_id),
   KEY `direction_id` (direction_id),
-  KEY `shape_id` (shape_id)
 );
 
 LOAD DATA LOCAL INFILE 'agency.txt' INTO TABLE agency FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES;
@@ -121,10 +113,6 @@ LOAD DATA LOCAL INFILE 'agency.txt' INTO TABLE agency FIELDS TERMINATED BY ',' O
 LOAD DATA LOCAL INFILE 'calendar.txt' INTO TABLE calendar FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES;
 
 LOAD DATA LOCAL INFILE 'calendar_dates.txt' INTO TABLE calendar_dates FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES;
-
-LOAD DATA LOCAL INFILE 'fare_attributes.txt' INTO TABLE fare_attributes FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES;
-
-LOAD DATA LOCAL INFILE 'fare_rules.txt' INTO TABLE fare_rules FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES;
 
 LOAD DATA LOCAL INFILE 'routes.txt' INTO TABLE routes FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES;
 
